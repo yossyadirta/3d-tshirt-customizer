b14a42e885706edbc9e8151ab18ac1a08c2222d7
yossyadirta <yossyadirta@gmail.com>
2023-04-18 11:55:57 +0700

    Add canvas files and canvas setup done

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
 
-const index = () => {
-  return <div>index</div>;
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
+
+const CanvasModel = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      {/* <CameraRig> */}
+      {/* <Backdrop /> */}
+      <Center>
+        <Shirt />
+      </Center>
+      {/* </CameraRig> */}
+    </Canvas>
+  );
 };
 
-export default index;
+export default CanvasModel;
